6ab4d216696e8f546ca9cacf7ae243d2d88e08d6
filipcernecky <43435930+filipcernecky@users.noreply.github.com>
2020-01-15 20:29:03 +0100

    Add files via upload

---
+++ src/main/java/hibernate/demo/DeleteDemo.java
@@ -0,0 +1,52 @@
+package hibernate.demo;
+
+import hibernate.entity.Instructor;
+import hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.OneToOne;
+
+public class DeleteDemo {
+    public static void main(String[] argz) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // get instructor by primary key/id
+            int theId = 2;
+            Instructor tempInstructor =
+                    session.get(Instructor.class, theId);
+            System.out.println("Found instructor: " + tempInstructor);
+
+            // delete the instructor
+            if (tempInstructor != null) {
+                System.out.println("Deleting: " + tempInstructor);
+
+                // associated "details" object will be deleted as well
+                session.delete(tempInstructor);
+            }
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
+
